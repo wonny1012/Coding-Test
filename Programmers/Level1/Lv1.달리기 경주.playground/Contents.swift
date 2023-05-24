be5734ff98ea7e6b15514905f76c07307a37f3e4
wonny1012 <rnjswldnjs1012@gmail.com>
2023-05-24 11:55:06 +0900

    Lv1.정수제곱

--- Programmers/Level1/Lv1.달리기 경주.playground/Contents.swift
+++ Programmers/Level1/Lv1.달리기 경주.playground/Contents.swift
@@ -7,19 +7,41 @@ import Foundation
 
 
 import Foundation
+//
+//func solution(_ players:[String], _ callings:[String]) -> [String] {
+//    var playingline = players
+//  for i in callings {
+//    // var player = playingline.firstIndex(of: i)
+//    var frontplayer = playingline.firstIndex(of: i)!-1
+//    playingline[playingline.firstIndex(of: i)!] = playingline[frontplayer]
+//    playingline[frontplayer] = i
+//
+//}
+//    return playingline
+//}
+//
+
+import Foundation
 
 func solution(_ players:[String], _ callings:[String]) -> [String] {
-    var playingline = players
-  for i in callings {
-    // var player = playingline.firstIndex(of: i)
-    var frontplayer = playingline.firstIndex(of: i)!-1
-    playingline[playingline.firstIndex(of: i)!] = playingline[frontplayer]
-    playingline[frontplayer] = i
- 
-}
-    return playingline
+    var sum : [Int] = []
+    var solution : [String] = players
+    for i in players {
+        
+        sum.append(callings.filter{ $0 == i}.count)
+        print(sum)
+    }
+    
+    for n in sum {
+        if n != 0 {
+            players
+        }
+    }
+    
+    return solution
 }
 
+solution(["mumu", "soe", "poe", "kai", "mine"], ["kai", "kai", "mine", "mine"])
 
 
 
