f458be82907e8d013acad8c3c4ebf7a94e2fab56
wonny1012 <rnjswldnjs1012@gmail.com>
2023-05-30 23:12:10 +0900

    Lv1.약수의개수와덧셈

--- Programmers/Level1/Lv1.약수의개수와덧셈.playground/Contents.swift
+++ Programmers/Level1/Lv1.약수의개수와덧셈.playground/Contents.swift
@@ -1,12 +1,29 @@
 import Foundation
 
-func solution(_ left:Int, _ right:Int) -> Int {
-    var num: [Int] = [left,right]
-    for i in num.min()!...num.max()! {
-        print(i)
-    }
+func solution(_ left: Int, _ right: Int) -> Int {
+    return (left...right).map { i in (1...i).filter { i % $0 == 0 }.count % 2 == 0 ? i : -i }.reduce(0, +)
+}
 
-    return 0
+
+
+func solution(_ left:Int, _ right:Int) -> Int {
+    var result: [Int] = []
+    var sum = 0
+    for i in left...right {
+        for j in 1...i {
+           if i%j == 0 {
+                result.append(j)
+            }
+        }
+        if result.count%2 == 0 {
+            sum += i
+        } else {
+            sum -= i
+        }
+        print(i,result)
+        result = []
+    }
+    return sum
 }
 
 solution(13,17)
