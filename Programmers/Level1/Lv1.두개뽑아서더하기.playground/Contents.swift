37cbe370db7811a520abca4c199b53fc2643fd04
wonny1012 <rnjswldnjs1012@gmail.com>
2023-05-31 17:50:51 +0900

    Lv1.두개뽑아서더하기

---
+++ Programmers/Level1/Lv1.두개뽑아서더하기.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var result:[Int] = []
+
+    for i in 0..<numbers.count {
+        for a in i+1..<numbers.count {
+            result.contains(numbers[i] + numbers[a]) ? nil : result.append(numbers[i] + numbers[a])
+        }
+    }
+    
+    return result.sorted()
+}
