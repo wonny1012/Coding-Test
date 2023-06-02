1179652761f694a1a1076fd49fc0af86923a9bfb
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-02 16:09:31 +0900

    Lv1.추억점수

---
+++ Programmers/Level1/Lv1.추억점수.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
+    let score: [String: Int] = Dictionary(uniqueKeysWithValues: zip(name, yearning))
+    return photo.map { $0.reduce(0) { $0 + (score[$1] ?? 0) } }
+}
+
+
+func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
+    var result:[Int] = []
+    for i in 0..<photo.count {
+        var sum = 0
+        var check = photo[i]
+        for k in 0..<name.count {
+            if check.contains(name[k]) {
+                sum += yearning[k]
+            }
+        }
+        result.append(sum)
+    }
+    return result
+}
+
+soultion(["may", "kein", "kain", "radi"],[5, 10, 1, 3],[["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])
