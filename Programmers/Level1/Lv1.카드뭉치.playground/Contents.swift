ae501a8a1e7671a2a72fca9592a1a67e04edb5f5
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-03 02:45:46 +0900

    Lv1.카드뭉치

---
+++ Programmers/Level1/Lv1.카드뭉치.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+func solution(_ cards1: [String], _ cards2: [String], _ goal: [String]) -> String {
+    var first = 0
+    var second = 0
+    var finalfirst = 0
+    var finalsecond = 0
+
+    for i in goal {
+        if cards1[first] == i {
+            if first == cards1.count-1 {
+                finalfirst = first + 1
+            } else {
+                first += 1
+                finalfirst += 1
+            }
+        } else if cards2[second] == i {
+            if second == cards2.count-1 {
+                finalsecond = second + 1
+            } else {
+                second += 1
+                finalsecond += 1
+            }
+        }
+    }
+    print(finalfirst,finalsecond)
+    
+    if finalfirst + finalsecond == goal.count {
+        return "Yes"
+    } else {
+        return "No"
+    }
+}
