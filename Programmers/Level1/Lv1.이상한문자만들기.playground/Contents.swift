8fb396a9aa8274f76a61a3dcff4656f7d51c2bcf
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-05 02:46:03 +0900

    Lv1.이상한문자만들기

--- Programmers/Level1/Lv1.이상한문자만들기.playground/Contents.swift
+++ Programmers/Level1/Lv1.이상한문자만들기.playground/Contents.swift
@@ -1,24 +1,34 @@
-
 func solution(_ s:String) -> String {
     var split = s.split(separator: " ")
-    var result:[String] = []
+    var result: [String] = []
+    var smallalphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","x","z"]
+    var bigalphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
     for i in 0..<split.count {
-
-        var mapping = split[i].map { $0 }
-        print(mapping)
-        
+        var mapping = split[i].map { String($0) }
         for j in 1...mapping.count {
-            if j%2 != 0 {
-                result.append(mapping[j-1].uppercased())
+            if j%2 == 1 {
+                //1,1 2,0 3,1 4,0
+                if smallalphabet.contains(mapping[j-1]) || bigalphabet.contains(mapping[j-1]) {
+                    result.append(mapping[j-1].uppercased())
+                } else {
+                    result.append(mapping[j-1])
+                }
+                
             } else {
-                result.append(String(mapping[j-1]))
+                 if smallalphabet.contains(mapping[j-1]) || bigalphabet.contains(mapping[j-1]) {
+                    result.append(mapping[j-1].lowercased())
+                } else {
+                    result.append(mapping[j-1])
+                }
+               
             }
         }
-        result.append(" ")
+        if i != split.count-1 {
+            result.append(" ")
+        }
     }
-    result.dropLast()
-    
     return result.joined()
 }
 
-solution(" try hello world")
+solution("    het  jea!!!!!@@#$#  ")
