08bde9066d3e3ba7c70ac78c044bf8dc92687464
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-12 17:18:42 +0900

    Lv2.최댓값과최솟값:

--- Programmers/Level1/Lv1.소수찾기.playground/Contents.swift
+++ Programmers/Level1/Lv1.소수찾기.playground/Contents.swift
@@ -1,32 +1,84 @@
 import Foundation
 
-func solution(_ n:Int) -> Int {
-    var resultArray: [Int] = [2,3]
-    if n == 2 {
-        resultArray = [2]
-    } else if n == 3 {
+var number:[Int] = []
+var sss = "3 5 6 0 -1 -8"
+//print(Array(sss))
+var a = sss.components(separatedBy: " ").map{ Int($0)! }
+print("\(a.max()!) \(a.min()!)")
+
+
+//var q:[String] = []
+//var solution:[Int] = []
+//
+//for i in pp {
+//    if i != " " {
+//        q.append(String(i))
+//    }
+//}
+//
+//while q.count != 0 {
+//    if q[0] == "-" {
+//        q[1] = "-" + q[1]
+//        q.removeFirst()
+//    } else {
+//        solution.append(Int(q[0])!)
+//        q.removeFirst()
+//    }
+//}
+//
+//print(solution.max()!," ",solution.min()!)
+//print(String(solution.max()!) + String(solution.min()!))
+//
         
-    } else {
-    for i in 4...n {
-        if i%2 == 0 {
-            
-        } else {
-            var num = 0
-            var doublei = Double(i)
-            for k in 2...Int(sqrt(doublei)) {
-                if i%k == 0 {
-                    num = 1
-                }
-            }
-            if num == 0 {
-                resultArray.append(i)
-            }
+//    var number:[Int] = []
+//    var result:[String] = []
+//
+//    for i in numberArray {
+//        if i.contains("-") {
+//            var array = i.map{ String($0) }
+//            number.append(Int(array[1])! * -1)
+//        } else {
+//            number.append(Int(i)!)
+//        }
+//    }
+//    var max = String(number.max()!)
+//    var min = String(number.min()!)
+//
+//
+//
+//    return min + " " + max
+//}
+//
+//solution("3 5 6 0 -1")
 
-        }
-    }
-
-    }
-    return resultArray.count
-}
-
-solution(10)
+//
+//func solution(_ n:Int) -> Int {
+//    var resultArray: [Int] = [2,3]
+//    if n == 2 {
+//        resultArray = [2]
+//    } else if n == 3 {
+//
+//    } else {
+//    for i in 4...n {
+//        if i%2 == 0 {
+//
+//        } else {
+//            var num = 0
+//            var doublei = Double(i)
+//            for k in 2...Int(sqrt(doublei)) {
+//                if i%k == 0 {
+//                    num = 1
+//                }
+//            }
+//            if num == 0 {
+//                resultArray.append(i)
+//            }
+//
+//        }
+//    }
+//
+//    }
+//    return resultArray.count
+//}
+//
+//solution(10)
---
+++ Programmers/Level2/Lv2.최댓값과 최솟값.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+func solution(_ s:String) -> String {
+    var numberArray = s.components(separatedBy: " ").map{ Int($0)! }
+    var min = String(numberArray.min()!)
+    var max = String(numberArray.max()!)
+    return min + " " + max
+}
