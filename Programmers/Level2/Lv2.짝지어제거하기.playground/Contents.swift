c0d25cc8c1f764910f28972611718de58ed2ef32
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-13 01:46:14 +0900

    Lv2.짝지어제거하기

---
+++ Programmers/Level2/Lv2.짝지어제거하기.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ s: String) -> Int {
+    var stack: [Character] = []
+    
+    for char in s {
+        if let top = stack.last, top == char {
+            stack.removeLast()
+        } else {
+            stack.append(char)
+        }
+    }
+    
+    return stack.isEmpty ? 1 : 0
+}
+
+
+solution("baaba")
