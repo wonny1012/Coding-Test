521a8ea28dbac5553e8887927998c5b4334761b8
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-13 11:32:09 +0900

    Lv2.카펫

---
+++ Programmers/Level2/Lv2.카펫.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+    var answer:[Int] = []
+    var check = false
+    var sum = brown + yellow
+    var num = 1
+    while check == false {
+        if sum%num == 0 {
+            var a = sum/num
+            var b = num
+            if (a-2)*(b-2) == yellow {
+                answer = [a,b]
+                check = true
+                break
+            } else {
+                num += 1
+            }
+        } else {
+            num += 1
+        }
+    }
+    return answer
+}
