b33d55a33a8acf9aa8754842ddcfdd472ac83dd6
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-24 12:50:45 +0900

    Icandoitgit add .

--- practice.playground/Contents.swift
+++ practice.playground/Contents.swift
@@ -1,5 +1,70 @@
 import Foundation
 
+
+
+func solution(_ n:Int) -> Int {
+    var a = String(n, radix: 3)
+    var aa = a.map{ String($0) }
+    aa.reverse()
+
+    
+    
+    return 0
+}
+
+solution(45)
+
+
+max(4, 5)
+min(4, 5)
+
+var ass = [1,2,4,5]
+ass.max()
+ass.min()
+
+func solution(_ s: String) -> String {
+    var answer = ""
+    let words = s.components(separatedBy: " ")
+
+    for word in words {
+        var convertedWord = ""
+        let characters = Array(word.lowercased())
+        print(characters)
+
+        for (index, char) in characters.enumerated() {
+            if index == 0 {
+                convertedWord += String(char).uppercased()
+            } else {
+                convertedWord += String(char)
+            }
+        }
+
+        answer += convertedWord + " "
+    }
+
+    answer.removeLast()
+    return answer
+}
+
+
+func solution(_ s:String) -> String {
+    var mapping = s.map{ String($0) }
+    var result:[String] = []
+    if s.count%2 == 0 {
+        for i in s.count/2-1 ... s.count/2 {
+            result.append(String(mapping[i]))
+        }
+    } else {
+        result.append(String(mapping[Int(s.count/2)]))
+    }
+    print(result)
+    var a =  result.joined()
+    return String(result.joined())
+}
+
+solution("abckde")
+
+
 func permute(_ nums: [Int], _ targetNum: Int) -> [[Int]] {
     var result = [[Int]]()
     var visited = [Bool](repeating: false, count: nums.count)
