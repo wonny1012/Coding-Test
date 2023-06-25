7922b1e9dcdf3dfffdebaf17bf2e806907b047d3
wonny1012 <rnjswldnjs1012@gmail.com>
2023-06-25 23:51:23 +0900

    addPractice

--- practice.playground/Contents.swift
+++ practice.playground/Contents.swift
@@ -1,6 +1,36 @@
 import Foundation
 
 
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var resultarray: [[Double]] = [ ]
+    for num in 1...N  {
+        var a = 0
+        var b = 0
+        for i in 0..<stages.count{
+             if num <= stages[i] {
+            b += 1
+             }
+            if num == stages[i] {
+                a += 1
+            }
+        }
+    
+        resultarray.append([Double(a)/Double(b),Double(num)])
+    }
+    print(resultarray)
+    // var mark = resultarray.sorted(by: { $0[0] > $1[0] })
+    resultarray.sort(by: { $0[0] > $1[0] })
+    print(resultarray)
+    var solution: [Int] = []
+    for q in 0..<resultarray.count {
+        solution.append(Int(resultarray[q][1]))
+    }
+    
+    return solution
+}
+
+solution(5,[2, 1, 2, 6, 2, 4, 3, 3])
+
 
 func solution(_ n:Int) -> Int {
     var a = String(n, radix: 3)
