9d5ca87f2149454e8f9cfb72163a45e7e1e978b3
wonny1012 <rnjswldnjs1012@gmail.com>
2023-11-21 00:17:04 +0900

    Lv1.숫자 문자열과 영단어

---
+++ Programmers/Level1/Lv1.숫자 문자열과 영단어.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ s:String) -> Int {
+    var result = ""
+    var dict = ["zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven":"7", "eight":"8", "nine":"9"]
+    var num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    var string = ""
+    var array = s.map{ String($0) }
+    for i in array {
+        string += i
+        // print(string)
+        if dict[string] != nil {
+            result += dict[string]!
+            // print(string)
+            string = ""
+        } else if num.contains(string) {
+            result += string
+            string = ""
+        }
+    }
+   
+    
+    return Int(result)!
+}
