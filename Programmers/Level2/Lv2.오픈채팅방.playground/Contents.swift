1b2fc3c80b7a882a599aed10c67a8fe75c6b61e6
wonny1012 <rnjswldnjs1012@gmail.com>
2023-11-22 01:38:55 +0900

    Lv2.오픈채팅방

---
+++ Programmers/Level2/Lv2.오픈채팅방.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func solution(_ record:[String]) -> [String] {
+    var recordArray = [[String]]()
+    var dicArray: [String: String] = [:]
+    var result = [String]()
+    var status = [String]()
+    
+    for i in record {
+        var array = (i.split(separator: " "))
+        recordArray.append([String(array[0]),String(array[1])])
+        status.append(String(array[0]))
+        if array.count == 3 {
+            dicArray[String(array[1])] = String(array[2])
+        }
+    }
+    for i in 0..<recordArray.count{
+        var id = recordArray[i][1]
+        if status[i] == "Enter" || status[i] == "Leave" {
+            result.append("\(dicArray[id]!)님이 \(status[i] == "Enter" ? "들어왔습니다." : "나갔습니다.")")
+        }
+    }
+    return result
+}
